65ad2357179bd1945e3809f0629d1d13d19ea89f
sareh <sareh.heidari@gmail.com>
2016-02-09 20:06:54 +0000

    User model

---
+++ models/user.js
@@ -0,0 +1,19 @@
+var mogoose = require('mongoose');
+var bcrypt  = require('bcrypt-nodejs');
+
+var userSchema = new mongoose.Schema({
+  local: {
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true }
+  }
+});
+
+userSchema.statics.encrypt = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
