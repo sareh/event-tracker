75fc01e5340a38ccf0bdedd1acb40939c91e3f3e
sareh <sareh.heidari@gmail.com>
2016-02-09 21:47:38 +0000

    Added routes

--- config/routes.js
+++ config/routes.js
@@ -3,10 +3,20 @@ var passport = require('passport');
 var router   = express.Router();
 
 var authenticationsController = require('../controllers/authenticationsController');
+var usersController = require('../controllers/usersController');
 var eventsController = require('../controllers/eventsController');
 
 router.post('/login', authenticationsController.login);
 router.post('/register', authenticationsController.register);
+
+router.route('/users')
+  .get(usersController.usersIndex)
+
+router.route('/users/:id')
+  .get(usersController.usersShow)
+  .put(usersController.usersUpdate)
+  .patch(usersController.usersUpdate)
+  .delete(usersController.usersDelete)
 
 router.route('/events')
   .get(eventsController.eventsIndex)
