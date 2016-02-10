4b361a0f412ec8ff88e0a5b739cf24f4b0183f63
sareh <sareh.heidari@gmail.com>
2016-02-10 11:39:59 +0000

    Current user service

---
+++ public/src/js/services/currentUser.js
@@ -0,0 +1,24 @@
+angular
+  .module('event-tracker')
+  .service('currentUser', CurrentUser);
+
+CurrentUser.$inject = ["TokenService"];
+
+function CurrentUser(TokenService){
+
+  var self  = this;
+  self.user = {};
+
+  self.saveUser = function(user){
+    self.user = user;
+  }
+
+  self.getUser = function(){
+    return self.user;
+  }
+
+  self.clearUser = function(){
+    return self.user = {};
+  }
+
+}
